c6bf60113300faee976c746dfe07ddea24cb8c1d
laowudui <laowudui@gmail.com>
2024-12-06 22:48:21 +0800

    feat: add PWA support and update UI
    - Add service worker registration for PWA support
    - Update 404 and index pages to use "App" title
    - Add favicon and meta tags for better web app experience

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 document.documentElement.lang = navigator.language;
+
+async function registerServiceWorker() {
+    if ("serviceWorker" in navigator) {
+        try {
+            let registration = await navigator.serviceWorker.register("/site.sw.js", {
+                scope: "/",
+            });
+            if (registration.installing) {
+                console.log("正在安装 Service worker");
+            } else if (registration.waiting) {
+                console.log("已安装 Service worker installed");
+            } else if (registration.active) {
+                console.log("激活 Service worker");
+            }
+        } catch (error) {
+            console.error(`注册失败：${error}`);
+        }
+    }
+}
+
+registerServiceWorker();
